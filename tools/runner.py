980c87d182ecd00ebdd9fd12f25571daa9aacfe2
Rogerlv51 <949608370@qq.com>
2023-08-05 13:19:31 +0800

    update some configs

--- tools/runner.py
+++ tools/runner.py
@@ -79,13 +79,10 @@ def run_net(args, config, train_writer=None, val_writer=None):
             data_time.update(time.time() - batch_start_time)
             npoints = config.dataset.train._base_.N_POINTS
             dataset_name = config.dataset.train._base_.NAME
-            if dataset_name == 'PCN':
+            if dataset_name == 'PCN' or dataset_name == 'Teeth':
                 partial = data[0].cuda()
                 gt = data[1].cuda()
-                if config.dataset.train._base_.CARS:
-                    if idx == 0:
-                        print_log('padding while KITTI training', logger=logger)
-                    partial = misc.random_dropping(partial, epoch) # specially for KITTI finetune
+                # partial = misc.random_dropping(partial, epoch) # specially for KITTI finetune
 
             elif dataset_name == 'ShapeNet':
                 gt = data.cuda()
@@ -192,7 +189,7 @@ def validate(base_model, test_dataloader, epoch, ChamferDisL1, ChamferDisL2, val
 
             npoints = config.dataset.val._base_.N_POINTS
             dataset_name = config.dataset.val._base_.NAME
-            if dataset_name == 'PCN':
+            if dataset_name == 'PCN' or dataset_name == 'Teeth':
                 partial = data[0].cuda()
                 gt = data[1].cuda()
             elif dataset_name == 'ShapeNet':
